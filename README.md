4da98834623b1940a80039ad6b24492881df86cc
Mac Schmitz <mac.schmitz@att.net>
2022-07-20 14:36:59 -0400

    returned to working files

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # hw-19-text-editor
-Your task is to build a text editor that runs in the browser. The app will be a single-page application that meets the PWA criteria. Additionally, it will feature a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser. The application will also function offline.
+
+## Description
+
+This is a single-page text editor Progressive Web Application that runs in the browser. It features a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser. The application also functions offline.
+
+## Table of Contents
+
+- [Link](#link)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+
+## Link
+
+[GitHub Repo](https://github.com/mschmitzzz/hw-19-text-editor)
+
+[Heroku Deployed App](https://obscure-citadel-67485.herokuapp.com/)
+
+## Installation
+
+This application can be run in the browser by visiting the Heroku link above. To run this application offline, you can visit the Heroku deployed link and click the `Install!` button in the top left.
+
+## Usage
+
+To use the Team Profile Builder, run the following command in your project folder and answer the questions when prompted:
+`node index.js`
+
+## License
+
+MIT License
+
+Copyright (c) [2022] [Mac Schmitz]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Questions
+
+[Visit my GitHub profile](https://github.com/mschmitzzz/)
+If you have any additional questions about this project, you can [email](mailto:mac.schmitz@att.net) me
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,40 +1,55 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('PUT to the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({id: 1, value: content});
+  console.log("PUT to the database");
+
+  const jateDB = await openDB("jate", 1);
+
+  const tx = jateDB.transaction("jate", "readwrite");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.put({ id: 1, value: content });
+
   const result = await request;
-  console.log('data saved to the database', result.value);
+  console.log("🚀 - data saved to the database", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// TODO: Add logic for a method that gets all the content from the database - DONE
 export const getDb = async () => {
-  console.log('GET from the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
+  console.log("GET from the database");
+
+  // Create a connection to the database database and version we want to use.
+  const jateDB = await openDB("jate", 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDB.transaction("jate", "readonly");
+
+  // Open up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Use the .getAll() method to get all data in the database.
   const request = store.get(1);
+
+  // Get confirmation of the request.
   const result = await request;
-  result
-    ? console.log('data retrieved from the database', result.value)
-    : console.log('data not found in the database');
-  return result?.value;  
+
+  result ? console.log("Data found.") : console.log("Data does not exist.");
+
+  return result?.value;
 };
 
 initdb();
